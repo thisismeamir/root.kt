c989cde56eb274300faa9c0d3af4213246e1f2f1
Kid A <thisismeamir@outlook.com>
2026-05-15 19:11:29 +0200

    [build] CI for github actions and build script development for publishing to maven.

--- rootkt-format/build.gradle.kts
+++ rootkt-format/build.gradle.kts
@@ -1,22 +1,71 @@
 plugins {
-    kotlin("jvm") version "2.3.20"
+    kotlin("jvm")
+    `maven-publish`
+    signing
+    java
+    id("com.gradleup.nmcp")
 }
 
-group = "com.thisismeamir"
-version = "unspecified"
+group = "io.github.thisismeamir"
+version = "0.0.1"
 
-repositories {
-    mavenCentral()
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = project.group.toString()
+            artifactId = "rootkt-format"
+            version = project.version.toString()
+
+            pom {
+                name.set("rootkt")
+                description.set("Kotlin library for reading and writing ROOT files.")
+                url.set("https://github.com/thisismeamir/root.kt")
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("thisismeamir")
+                        name.set("Kid A")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/thisismeamir/root.kt")
+                    connection.set("scm:git:git://github.com/thisismeamir/root.kt.git")
+                    developerConnection.set("scm:git:ssh://github.com/thisismeamir/root.kt.git")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("SIGNING_KEY"),
+        System.getenv("SIGNING_PASSWORD")
+    )
+    sign(publishing.publications["mavenJava"])
+}
+
