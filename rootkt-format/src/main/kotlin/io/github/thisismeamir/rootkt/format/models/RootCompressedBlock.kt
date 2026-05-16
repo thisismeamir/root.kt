72ed4695004c68fbdd86d015e5a27ca6554e4235
Kid A <thisismeamir@outlook.com>
2026-05-16 14:45:00 +0200

    [implementation] introducing CompressedBlock to format module; this is where it is going to be used.

---
+++ rootkt-format/src/main/kotlin/io/github/thisismeamir/rootkt/format/models/RootCompressedBlock.kt
@@ -0,0 +1,10 @@
+package io.github.thisismeamir.rootkt.format.models
+
+import io.github.thisismeamir.rootkt.compression.models.CompressedBlock
+
+
+data class RootCompressedBlock(
+    val compressedBlock : CompressedBlock
+)
+
+
