cf860389bc07440853778f87a22a874fc25901d8
Kid A <thisismeamir@outlook.com>
2026-07-30 14:42:43 +0330

    [implementation] basic reading of the TDirectory

---
+++ rootkt-format/src/main/kotlin/io/github/thisismeamir/rootkt/format/models/TDirectory.kt
@@ -0,0 +1,13 @@
+package io.github.thisismeamir.rootkt.format.models
+
+data class TDirectory(
+    val version: Short,
+    val datimeC: Int,
+    val datimeM: Int,
+    val nbytesKeys: Int,
+    val nbytesName: Int,
+    val seekDir: Long,
+    val seekParent: Long,
+    val seekKeys: Long,
+    val uuid: ByteArray?
+)
